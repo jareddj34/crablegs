c6a3553424b095b1d6fef024b0d2fbb384994247
Jared <jareddj34@gmail.com>
2023-06-14 22:40:06 -0700

    some info stuff

---
+++ pages/info.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import Head from 'next/head';
+
+export default function Info() {
+
+    return (
+        <div>
+            <Head>
+                <title>User info page</title>
+            </Head>
+            <h1>
+                Tell us about yourself
+            </h1>
+        </div>
+    )
+}
